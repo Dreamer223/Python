451beaf247047fa24f17625af4ae4740987471d6
Dreamer223 <drackfil@gmail.com>
2023-06-18 19:55:36 +0300

    Task4

--- HomeWork/Task4.py
+++ HomeWork/Task4.py
@@ -5,3 +5,11 @@
 
 # 3 2 4 -> yes
 # 3 2 1 -> no
+
+n = int(input("По горизонтали: "))
+m = int(input("По вертикали: "))
+k = int(input("Сколько долек надо: "))
+if k < n * m and ((k % n == 0) or (k % m == 0)):
+    print('YES')
+else:
+    print('NO')
